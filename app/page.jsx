c64f5c3682ddf1a206884558247cebbdd6031882
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-04-23 00:50:49 +0200

    home page ui

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,18 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Welcome to Promote Generator
+        <br className="max-md:hidden" />
+        <span className="text-2xl orange_gradient text-center">
+          AI-Powered 🚀
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Promote Generator is a tool that helps you great social media posts for
+        your business.
+      </p>
+    </section>
+  );
 };
 export default Home;
