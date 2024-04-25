7ac6cff3efd66e129199626317b44eae552b1434
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-04-26 01:28:44 +0200

    redo

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,20 @@
+import Feed from '@components/Feed';
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Hello Sham 🫶🏼
+        Welcome to Learner
         <br className="max-md:hidden" />
         <span className="text-2xl orange_gradient text-center">
-          AI-Powered Boyfriend 🚀
+          AI-Powered 🚀
         </span>
       </h1>
       <p className="desc text-center">
-        <span className="blue_gradient font-bold">Your </span>
-        personalized AI-powered boyfriend.
+        <span className="blue_gradient font-bold">Learner </span>
+        is a tool that helps you to save your Ai-prompts and share them with the
+        world. You can also explore the prompts shared by other users.
       </p>
-      {/* <Feed /> */}
-      MMMMM.....
+      <Feed />
     </section>
   );
 };
